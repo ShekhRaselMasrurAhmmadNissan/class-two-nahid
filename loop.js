d06051e88c5859a15c76bff66c2a8dc08ffc2543
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2023-04-12 11:28:48 +0600

    for and while loop.

---
+++ loop.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 5; i++) {
+	// 0 theke 5 er kom porjonto cholbe.
+	if (i === 2) {
+		// break;
+		continue;
+	}
+	console.log('i = ', i);
+}
+
+// let j = 0;
+// while (j != 'n') {
+// 	console.log('j = ', j);
+// 	// Check the condition.
+// 	j++;
+// }
